3e9254aa2ad938a8837a3ff2cd5a6322dc356334
Tugran Demirel <demireltugran66@gmail.com>
2022-12-01 00:58:08 +0300

    code fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 
 // Routes import
 const userRoute = require('./routes/user')
-const autRoute = require('./routes/auth')
+const authRoute = require('./routes/auth')
 
 const app = express();
 dotenv.config();
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 // usage of userRoute
 app.use('/api/users', userRoute);
-app.use('/api/auth', autRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend server is running ')
