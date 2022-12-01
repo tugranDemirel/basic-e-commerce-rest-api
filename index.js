39412b2890a5d3787ca464b987f0b75e34e4ded9
Tugran Demirel <demireltugran66@gmail.com>
2022-12-01 03:46:30 +0300

    import routes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 const app = express();
 dotenv.config();
@@ -21,6 +23,8 @@ app.use(express.json())
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend server is running ')
